bc87d68086cf70ab47a0d980e87555b8568295e8
杨振华 <1259176311@qq.com>
2022-06-28 11:48:54 +0800

    User

---
+++ src/main/java/com/cqu/online_learning/entity/User.java
@@ -0,0 +1,27 @@
+package com.cqu.online_learning.entity;
+
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.ToString;
+
+import java.security.PrivateKey;
+import java.sql.Timestamp;
+
+@Data
+@ToString
+@TableName("edu_user")
+public class User {
+    @TableId(type = IdType.AUTO)
+    private Integer userId;
+    private Integer mobile;
+    private String email;
+    private String password;
+    private String showName;
+    private String sex;
+    private Timestamp createTime;
+    private String permission;
+    private String picImg;
+}
