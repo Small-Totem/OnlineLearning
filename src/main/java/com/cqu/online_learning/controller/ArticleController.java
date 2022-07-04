6ab7ca2e3fc1f0ff88130a0cb9934721a3a19966
Small-Totem <Small-Totem@users.noreply.github.com>
2022-07-04 15:01:35 +0800

    update dependency

--- src/main/java/com/cqu/online_learning/controller/ArticleController.java
+++ src/main/java/com/cqu/online_learning/controller/ArticleController.java
@@ -4,10 +4,7 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.cqu.online_learning.entity.Article;
 import com.cqu.online_learning.services.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,8 +34,20 @@ public class ArticleController {
         articleService.removeArticle(id);
         return "success";
     }
+
     @GetMapping("/queryArticlePage/{id}")//分页查询Article
     public IPage<Article> queryArticlePage(@PathVariable int id){
         return articleService.queryArticlePage(id,2);
     }
+
+    //test
+    @CrossOrigin(origins={"http://localhost:3000","http://localhost:8081"})//允许跨域访问 @ref https://www.csdn.net/tags/MtjaIg0sMzYzMC1ibG9n.html
+    @GetMapping("/article_test")
+    public String article_test(){
+        System.out.println("/article_test");
+        return "{\"code\":0,\"data\":[{\"id\":1,\"title\":\"2022年3月30号\"},{\"id\":72,\"title\":\"Vue.js 3.0\"},{\"id\"" +
+                ":75,\"title\":\"应用 & 组件实例\"},{\"id\":74,\"title\":\"测试Markdown语法\"},{\"id\":71,\"title\":\"测试" +
+                "图片\"},{\"id\":73,\"title\":\"测试评论\"}],\"msg\":\"成功\"}";
+    }
+
 }
