391cf74f306e1895df8fdc8f22be58f178e700a2
CQUcyx <1464073937@qq.com>
2022-07-05 09:46:27 +0800

    add function

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -2,90 +2,155 @@
     <div class="login-wrap">
         <div class="ms-login">
             <div class="ms-title">登陆到在线教育系统</div>
-            <el-form :model="param" :rules="rules" ref="login" label-width="0px" class="ms-content">
+          <el-tabs v-model="tabName" type="border-card" :stretch="true">
+            <el-tab-pane label="登录" name="login">
+              <el-form ref="loginForm" :model="loginForm">
                 <el-form-item prop="username">
-                    <el-input v-model="param.username" placeholder="username">
-                        <template #prepend>
-                            <el-button icon="el-icon-user"></el-button>
-                        </template>
-                    </el-input>
+                  <el-input
+                      v-model="loginForm.username"
+                      type="text"
+                      placeholder="请输入手机号"
+                      autocomplete="off"
+                      prefix-icon="el-icon-user"
+                  />
                 </el-form-item>
                 <el-form-item prop="password">
-                    <el-input type="password" placeholder="password" v-model="param.password"
-                        @keyup.enter="submitForm()">
-                        <template #prepend>
-                            <el-button icon="el-icon-lock"></el-button>
-                        </template>
-                    </el-input>
+                  <el-input
+                      v-model="loginForm.password"
+                      type="password"
+                      placeholder="请输入密码"
+                      autocomplete="off"
+                      prefix-icon="el-icon-lock"
+                  />
                 </el-form-item>
-                <div class="login-btn">
-                    <el-button type="primary" @click="submitForm()">登录</el-button>
-                </div>
-              <div class="login-btn">
-                <el-button type="primary" @click="submitForm()">注册</el-button>
-              </div>
-                <p class="login-tips">Tips : 用户名和密码随便填。</p>
-            </el-form>
+                <el-form-item>
+                  <el-button :loading="loading" type="primary" style="width: 100%" @click="login">登  录</el-button>
+                </el-form-item>
+              </el-form>
+            </el-tab-pane>
+            <el-tab-pane label="注册" name="register">
+              <el-form ref="regForm" :model="regForm">
+                <el-form-item prop="username">
+                  <el-input
+                      v-model="regForm.username"
+                      type="text"
+                      placeholder="请输入手机号"
+                      autocomplete="off"
+                      prefix-icon="el-icon-user"
+                  />
+                </el-form-item>
+                <el-form-item prop="password">
+                  <el-input
+                      v-model="regForm.password"
+                      type="password"
+                      placeholder="请输入密码"
+                      autocomplete="off"
+                      prefix-icon="el-icon-lock"
+                  />
+                </el-form-item>
+                <el-form-item prop="checkPassword">
+                  <el-input
+                      v-model="regForm.confirmPassword"
+                      type="password"
+                      placeholder="请确认密码"
+                      autocomplete="off"
+                      prefix-icon="el-icon-lock"
+                  />
+                </el-form-item>
+                <el-form-item>
+                  <el-button :loading="loading" type="primary" style="width: 100%" @click="register">注  册</el-button>
+                </el-form-item>
+              </el-form>
+            </el-tab-pane>
+          </el-tabs>
         </div>
     </div>
 </template>
 
 <script>
-import { ref, reactive } from "vue";
-import { useStore } from "vuex";
-import { useRouter } from "vue-router";
+
 import { ElMessage } from "element-plus";
+import axios from 'axios'
 
 export default {
-    setup() {
-        const router = useRouter();
-        const param = reactive({
-            username: "admin",
-            password: "123123",
-        });
+  data() {
+    return {
+      loginFormVisible: false,
+      loginForm: {
+        username: '13579246810',
+        password: '123abc'
+      },
+      regForm: {
+        username: '',
+        password: '',
+        confirmPassword: ''
+      },
+      tabName: 'login',
+      loading: false,
+      loginflag:-1,
+      registerFlag:"fail",
+      regParam:{
+        mobile:"",
+        password:""
+      },
+    }
+  },
+  methods: {
+    // 点击登录按钮
+    async login() {
+      const _this = this;
+      const url = 'http://localhost:8080/loginVerify/'+_this.loginForm.username+'/'+_this.loginForm.password
 
-        const rules = {
-            username: [
-                {
-                    required: true,
-                    message: "请输入用户名",
-                    trigger: "blur",
-                },
-            ],
-            password: [
-                { required: true, message: "请输入密码", trigger: "blur" },
-            ],
-        };
-        const login = ref(null);
-        const submitForm = () => {
-            login.value.validate((valid) => {
-                if (valid) {
-                    ElMessage.success("登录成功");
-                    localStorage.setItem("ms_username", param.username);
-                    router.push("/");
-                } else {
-                    ElMessage.error("登录成功");
-                    return false;
-                }
-            });
+      axios.post(url).then(function (res) {
+        //处理回传数据。
+        console.log(res)
+        _this.loginflag=res.data
+        if(_this.loginflag == -1) {
+          ElMessage({type: 'error', message: '用户名或密码错误!', showClose: true})
+        }
+        else{
+          ElMessage({type: 'true', message: '登陆成功!', showClose: true})
+          localStorage.setItem("ms_username", _this.loginForm.username);
+          _this.$router.push('/')
+
+        }
+      });
+      },
+      //注册
+      async register() {
+        const _this = this;
+        const url = 'http://localhost:8080/addUser'
+        if(_this.regForm.password!=_this.regForm.confirmPassword){
+          ElMessage({type: 'error', message: '两次输入的密码不一致!', showClose: true})
+        }else{
+          var queryInfo = new URLSearchParams() ;
+          queryInfo.append("mobile",_this.regForm.username)
+          queryInfo.append("password",_this.regForm.password)
+          console.log(queryInfo)
+        axios.post(url,queryInfo).then(function (res) {
+          //处理回传数据。
+          _this.registerFlag=res.data
+          if(_this.registerFlag =="success"){
+            ElMessage({type: 'true', message: '注册成功!', showClose: true})
+            localStorage.setItem("ms_username", _this.regForm.username);
+            _this.$router.push('/MainPage')
+          }else{
+            ElMessage({type: 'true', message: '服务器连接失败!', showClose: true})
+          }
+        })
         };
 
-        const store = useStore();
-        store.commit("clearTags");
-
-        return {
-            param,
-            rules,
-            login,
-            submitForm,
-        };
     },
+  },
+
+
+
 };
 </script>
 
 <style scoped>
 .login-wrap {
-    position: relative;
+    position: absolute;
     width: 100%;
     height: 100%;
     background-image: url(../assets/img/login-bg.jpg);
@@ -96,8 +161,9 @@ export default {
     line-height: 50px;
     text-align: center;
     font-size: 20px;
-    color: #fff;
+    color: #222222;
     border-bottom: 1px solid #ddd;
+    background-color: #cfe9ff;
 }
 .ms-login {
     position: absolute;
@@ -112,17 +178,11 @@ export default {
 .ms-content {
     padding: 30px 30px;
 }
-.login-btn {
-    text-align: center;
-}
+
 .login-btn button {
     width: 100%;
     height: 36px;
     margin-bottom: 10px;
 }
-.login-tips {
-    font-size: 12px;
-    line-height: 30px;
-    color: #fff;
-}
+
 </style>
